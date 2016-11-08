f7e157de51c736f189041c95fc78e63de6d6e909
cambele <tapiaveracamila@gmail.com>
2016-11-08 08:43:34 -0400

    Update hola.py

--- hola.py
+++ hola.py
@@ -4,3 +4,19 @@ print "chao"
 # Camila aporta una funcion suma
 def suma(a,b)
   return a+b
+
+def resta(c,d)
+  return c-d  
+
+def multiplicacion (e,f)
+  return e*f
+
+def min (g,h)
+  if g<h:
+    return g
+  else:
+    return h
+  
+def div (i,j)
+  return i/j
+
